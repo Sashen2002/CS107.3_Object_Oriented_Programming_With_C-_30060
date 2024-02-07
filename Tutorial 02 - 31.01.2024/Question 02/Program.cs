51f4369bfced2ef7e3089e6221074d4db9f00556
Sashen2002 <135606712+Sashen2002@users.noreply.github.com>
2024-02-07 13:44:45 +0530

    Add files via upload

---
+++ Tutorial 02 - 31.01.2024/Question 02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_02
+{
+    internal class Program
+    {
+        class BankAccount
+        {
+            public int AccountNumber;
+            public double Balance;
+
+            public void Deposit()
+            {
+                double i;
+                Console.WriteLine("Enter the Amount you need to deposit: ");
+                while (!double.TryParse(Console.ReadLine(), out i) || i<=0)
+                {
+                    Console.WriteLine("Not a Valid Amount!");
+                }
+                Balance += i;
+                Console.WriteLine("New Balance: " + Balance);
+            }
+        }
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount = new BankAccount();
+            bankAccount.Balance = 1000;
+            Console.WriteLine("Enter your Account number: ");
+            while (!int.TryParse(Console.ReadLine(), out bankAccount.AccountNumber))
+            {
+                Console.WriteLine("\nEnter a Valid Account Number!\n");
+            }
+            bankAccount.Deposit();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tutorial 02 - 31.01.2024/Question 05/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_05
+{
+    internal class Program
+    {
+        class LibraryBook
+        {
+            public string Title;
+            public string Author;
+            public bool Available;
+
+            public void BorrowBook(LibraryBook y)
+            {
+                CheckAvailabilty(y);
+                if (Available)
+                {
+                    Console.WriteLine("You have borrowed the book.\n");
+                    Available = false;
+                }
+                else
+                {
+                    Console.WriteLine("The book has already been taken.\n");
+                }
+            }
+            public LibraryBook(string x, string y, bool z)
+            {
+                Title = x;
+                Author = y;
+                Available = z;
+            }
+        }
+        static void CheckAvailabilty(LibraryBook x)
+        {
+            Console.WriteLine("Book: " + x.Title + "\nAuthor: " + x.Author + "\nAvailability: " + x.Available +"\n\n");
+        }
+        
+        static void Main(string[] args)
+        {
+            LibraryBook book = new LibraryBook("Harry Potter","J K Rowling",true);
+            LibraryBook book1 = new LibraryBook("Madolduwa", "Martin Wickramasinghe", true);
+            LibraryBook book2 = new LibraryBook("Sherlock Holmes", "Arthur Conan Doyle", false);
+
+            book.BorrowBook(book);
+            book1.BorrowBook(book1);
+            book2.BorrowBook(book2);
+
+            CheckAvailabilty(book);
+            CheckAvailabilty(book1);
+            CheckAvailabilty(book2);
+
+            Console.ReadLine();
+        }
+    }
+}
